bb6cd1da9037c8480a2b1a971c64bdf4f11b0fc3
MarcellKov <kmarcell379@gmail.com>
2023-04-01 08:37:15 +0200

    MM

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,20 @@
 <script>
 	import "gun"
+	async function requestAccount() {
+
+if(window.ethereum){
+	try {
+		const accounts = await window.ethereum.request({
+			method: "eth_requestAccounts",
+		});
+		window.ethereum.request({method:"eth_"})
+		console.log(accounts)           
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+}
 	const gun=Gun({peers:["http:localhost:5200/gun"],})
 	var k
 	var g
@@ -9,3 +24,4 @@
 <h1>Üdv a DeChatban</h1>
 <input bind:value={k} on:input={gun.get("message").put({"data":k})}>
 <h1>{g}</h1>
+<button on:click={requestAccount()}>AAA</button>
