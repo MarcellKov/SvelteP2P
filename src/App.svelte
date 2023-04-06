1b6d78c29d2084f1f57be61db5714131bd85e17c
MarcellKov <kmarcell379@gmail.com>
2023-04-06 20:12:46 +0200

    CSS

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,28 @@
 <script>
 	import "gun"
-	
-	
-	import {requestAccount} from "./webfunctions"
-	const gun=Gun({peers:["http:localhost:5200/gun"],})
-	var k
-	var g
-	gun.get("message").on((a)=>{g=a["data"]})
+	import Register from "./Register.svelte";
+
 </script>
 
-<h1>Üdv a DeChatban</h1>
-<input bind:value={k} on:input={gun.get("message").put({"data":k})}>
-<h1>{g}</h1>
-<button on:click={requestAccount}>METAMASK LOGIN</button>
+
+
+<div class="landing_container">
+<div class="landing_welcome">
+<div class="welcome_body">
+
+<input type="text" placeholder="Username" id="inpb">
+<div class="line">
+<input type="password" placeholder="Password" id="pw">
+<button on:click={()=>{document.getElementById("pw").type="text"}}></button>
+</div>
+<button>LOGIN</button>
+
+</div>
+</div>
+</div>
+
+
+
+
+
+
